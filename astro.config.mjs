9b3f8ee0badac7c6522b6e142a86db3c94cabd4c
Amman <amman@valourone.com>
2025-08-21 16:42:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
-// NOTE: Using custom domain (valourone.com), so no `base` here.
 export default defineConfig({
   site: 'https://valourone.com',
   integrations: [tailwind(), sitemap(), mdx()],
